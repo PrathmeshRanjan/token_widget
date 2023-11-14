c285265c2ec71b4586f58dd8a488043d0b4cd7a3
PrathmeshRanjan <21uec101@lnmiit.ac.in>
2023-11-15 01:01:27 +0530

    Changes in UI

--- coingecko-widget.js
+++ coingecko-widget.js
@@ -1,5 +1,7 @@
 const script = document.currentScript;
-const tokenName = script.getAttribute("data-token");
+let tokenName = prompt(
+  "Enter the name of the cryptocurrency token in lowercase (e.g. bitcoin):"
+);
 
 if (!tokenName) {
   console.error(
@@ -15,11 +17,17 @@ fetch(apiUrl)
   .then((data) => {
     const widgetContainer = document.createElement("div");
     widgetContainer.innerHTML = `
-                <h2>${data.name} Details</h2>
+                <h1>CoinGecko Cryptocurrency Widget</h1>
+                <h2>${data.name} Details:</h2>
+                <img src="${data.image.small}" alt="">
+                <p>Description: ${data.description.en}</p>
                 <p>Market Cap: $${data.market_data.market_cap.usd}</p>
                 <p>Current Price: $${data.market_data.current_price.usd}</p>
                 <p>24h Trading Volume: $${data.market_data.total_volume.usd}</p>
-            `;
+                <div id="footer">
+                    Data provided by <a href="https://www.coingecko.com/" target="_blank">CoinGecko</a>
+                </div>
+                `;
 
     // Append the widget to the body
     document.body.appendChild(widgetContainer);
